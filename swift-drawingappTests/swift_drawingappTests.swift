f12c3f5bfad422a8e884008ba61761a94999445e
jay.choi <jay.choi@kakaocorp.com>
2022-07-04 16:27:43 +0900

    test succeed - add rectangles
    - Drawers 모델에서 Rectangle을 추가
    - 추가된 Rectangle은 서로 다른 ID를 가짐

--- swift-drawingappTests/swift_drawingappTests.swift
+++ swift-drawingappTests/swift_drawingappTests.swift
@@ -23,11 +23,13 @@ class DrawersTest: XCTestCase {
 
     func testCreateRectangles() {
         drawers.addRectangle()
+        drawers.addRectangle()
         
-        XCTAssertEqual(drawers.countOfShapes, 1)
+        XCTAssertEqual(drawers.countOfShapes, 2)
         
-        let shape = drawers.shape(of: 0) as! Rectangle
-        
+        let shape1 = drawers.shape(of: 0) as! Rectangle
+        let shape2 = drawers.shape(of: 1) as! Rectangle
+        XCTAssertNotEqual(shape1.id, shape2.id)
     }
 
 }
