90d5a27304f5c552285729c927808dd9d9a7bf9a
jake-1022 <csh8130@kakao.com>
2022-07-06 20:27:11 +0900

    Color, DrawObject, Rectangle class 생성

---
+++ swift-drawingapp/Model/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  swift-drawingapp
+//
+//  Created by main on 2022/07/06.
+//
+
+import Foundation
+
+final class Color {
+    var red: Float = 0.0
+    var green: Float = 0.0
+    var blue: Float = 0.0
+}
---
+++ swift-drawingapp/Model/DrawObject.swift
@@ -0,0 +1,12 @@
+//
+//  DrawObject.swift
+//  swift-drawingapp
+//
+//  Created by main on 2022/07/06.
+//
+
+import Foundation
+
+class DrawObject {
+    var color: Color?
+}
