0715c765591425ede6cb474c983541130221022e
Jung Kim <godrm77@gmail.com>
2022-07-07 10:41:14 +0900

    Revert "사각형, 라인 드로잉을 하나의 Draw라는 것으로 추상화 하고  protocol로 기능들을 분류, (#11)"
    This reverts commit 033a27106a21c6bf881c1d7f40b64dab87ad763b.

--- swift-drawingapp/ViewController.swift
+++ swift-drawingapp/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var color: UIColor = .systemRed
         // Do any additional setup after loading the view.
     }
 
