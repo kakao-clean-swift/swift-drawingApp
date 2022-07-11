b102261dcf6dac1cda75776129038874167d98c5
jinny <jinny.jo@kakaocorp.com>
2022-07-11 15:48:49 +0900

    LineDrawingTests 깨지는 것 수정.

--- Domain/Tests/EntityTests/LineDrawingTests.swift
+++ Domain/Tests/EntityTests/LineDrawingTests.swift
@@ -6,12 +6,12 @@ class LineDrawingTests: XCTestCase {
     func test_line_drawing_append() {
         // given
         var sut = LineDrawing(initialPoint: .zero)
-        XCTAssertTrue(sut.points.isEmpty)
+        XCTAssertEqual(sut.points.count, 1)
         
         // when
         sut.addPoint(CGPoint(x: 10, y: 10))
         
         // then
-        XCTAssertFalse(sut.points.isEmpty)
+        XCTAssertEqual(sut.points.count, 2)
     }
 }
